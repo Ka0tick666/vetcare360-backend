5c9f231546366c189ec31dc9bb19098d92592bae
Ka0tick666 <benjaminnavarretessg1@gmail.com>
2026-09-15 00:34:30 -0300

    feat: permitAll on /api/health in SecurityConfig

--- src/main/java/com/vetcare360/backend/SecurityConfig.java
+++ src/main/java/com/vetcare360/backend/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.vetcare360.backend;
+package com.vetcare360.backend;//ola
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
