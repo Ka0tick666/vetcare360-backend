aa314426bf1f7a5cc6822eac28df48530533034a
Ka0tick666 <benjaminnavarretessg1@gmail.com>
2026-09-14 23:17:58 -0300

    fix api6

--- src/main/java/com/vetcare360/backend/BackendApplication.java
+++ src/main/java/com/vetcare360/backend/BackendApplication.java
@@ -2,12 +2,8 @@ package com.vetcare360.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.vetcare360.backend", "com.vetcare360.products"})
-@EnableJpaRepositories(basePackages = "com.vetcare360.products")
-@EntityScan(basePackages = "com.vetcare360.products.entity")
 public class BackendApplication {
 
     public static void main(String[] args) {
